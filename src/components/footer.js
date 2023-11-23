e59088f5d40ed5a6834c4aa0830cf1dd27cd43d0
Iyan  Godwin <129687090+Ventureplace@users.noreply.github.com>
2023-11-22 20:54:37 -0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+         for Netlify CMS, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
